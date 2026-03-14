9f208beb0a5a090913e48bbd9e23d3bd9dcbf504
juahn Shin <juahn112@gmail.com>
2026-03-14 23:58:36 +0900

    practice

--- practice_quest/DCU_code.py
+++ practice_quest/DCU_code.py
@@ -1,6 +1,5 @@
 original_price = int(input())
 if original_price > 50000:
     discount = original_price * 0.95
-elif original_price
-    discount = 0
+elif original_price > 100000:
 print(discount)
