252212e830537aacf28a44f062f5e48887d97d98
juahn Shin <juahn112@gmail.com>
2026-03-14 23:36:54 +0900

    practice

--- practice_quest/DCU_code.py
+++ practice_quest/DCU_code.py
@@ -1,5 +1,10 @@
-a, b, c = map(int, input("Input 3 integer values>>").split())
-if a + b > c and a + c > b and b + c > a:
-    print("Yes, this is a triangle.")
-else:
-    print("No, this is NOT a triangle.")
+money = int(input())
+count_500 = money // 500
+money = money % 500
+count_100 = money // 100
+money = money % 100
+count_50 = money // 50
+money = money % 50
+count_10 = money // 10
+money = money % 10
+print(count_500, count_100, count_50, count_10)
